472249d7d6aff2f8264fbdc446bfc4fc31a949b3
adarshswaminath <adarshswaminath7@gmail.com>
2023-12-17 18:18:55 +0530

    feature: new neo brutalist design added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const linkStyle = 'underline-offset-2 hover:underline hover:decoration-wavy decoration-pink-600 transition ease-linear'
+const linkStyle = 'underline-offset-2 hover:underline hover:decoration-wavy decoration-[#ff4911] transition ease-linear'
 
 function Navbar() {
   return (
-    <div className=' text-white  text-center right-0 flex items-center justify-end p-2 gap-3 font-medium z-50 text-xl'>
+    <div className='bg-white text-black  text-center right-0 flex items-center justify-end p-3 h-14 border-b-4 border-black gap-3 font-medium z-50 text-xl'>
         <a className={`${linkStyle}`} href="/">Home</a>
         <a className={`${linkStyle}`} href="/about">About</a>
         <a className={`${linkStyle}`} href="#">Contact</a>
