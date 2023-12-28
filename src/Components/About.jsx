ac0a4b68d071d444d0c808f7fad9be74816b2be8
adarshswaminath <adarshswaminath7@gmail.com>
2023-12-28 11:41:00 +0530

    featre: scroll top added

---
+++ src/Components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center'>
+        <h2 className='text-3xl font-bold'>About Me</h2>
+        <p>Hi Iam a self learned developer</p>
+    </div>
+  )
+}
+
+export default About
