1587970e8d08eb1797d94ec9f2b40fe3dc63c0ba
adarshswaminath <adarshswaminath7@gmail.com>
2023-12-30 14:08:34 +0530

    feature: blob curosr and structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import Hero from './Layouts/Hero'
 
 function App() {
+  
   return (
-    <div>App</div>
+    <div>
+      <div className='scroll-watcher fixed h-[5px] rounded-r-full bg-gradient-to-r from-pink-500 to-indigo-500 top-0 w-full z-50'></div>
+      <Hero/>
+      <div className="min-h-screen"></div>
+    </div>
   )
 }
 
--- src/Components/Cursor.tsx
+++ src/Components/Cursor.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from "react"
+ interface CursorProps {
+    x: number;
+    y: number;
+ }
 
-function Cursor() {
+const Cursor: React.FC<CursorProps> = ({x,y}) => {
   return (
-    <div>Cursor</div>
+    <div style={{
+        left: x,
+        top: y,
+    }} className='wrapper  bg-white rounded-full absolute transform -translate-x-1/2 -translate-y-1/2 mix-blend-difference'>
+        <p><span></span></p>
+        <p><span></span></p>
+    </div>
   )
 }
 
