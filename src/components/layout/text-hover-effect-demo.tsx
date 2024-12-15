f157369b61e60e66148a6a75709255f1e686218f
adarshswaminath <adarshswaminath7@gmail.com>
2024-12-15 17:19:59 +0530

    feat: new work progress page addeed

---
+++ src/components/layout/text-hover-effect-demo.tsx
@@ -0,0 +1,9 @@
+import { TextHoverEffect } from "../ui/text-hover-effect";
+
+export function TextHoverEffectDemo() {
+  return (
+    <div className="h-screen bg-black flex items-center justify-center">
+      <TextHoverEffect text="Work in progress " />
+    </div>
+  );
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#E1E2D9",
+		primaryText: "#1A1B1A",
+      },
+	  fontFamily: {
+		sans: ["Noto Sans", "sans-serif"],}
+    },
+  },
+  plugins: [addVariablesForColors],
+};
+
+
+function addVariablesForColors({ addBase, theme }) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+	  Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+   
+	addBase({
+	  ":root": newVars,
+	});
+  }
